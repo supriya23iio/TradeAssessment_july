f8b8f1d2cd728f00297c2f763027fdca3f9a4a3f
supriya23iio <76202136+supriya23iio@users.noreply.github.com>
2024-07-16 23:44:44 +0530

    Delete TradeAssessment_DB directory

--- TradeAssessment_DB/tradestorage/tradestorage/src/main/java/com/db/tradestorage/dao/TradeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.db.tradestorage.dao;
-
-import com.db.tradestorage.model.Trade;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TradeRepository extends JpaRepository<Trade,String> {
-}
